c2f1cf6d95a487d5da81b1d0c038e0d3c27fa195
CarlosHuamaniEstudianteTec <carlos.huamani.pi@tecsup.edu.pe>
2025-04-15 22:27:21 -0500

    Revert "feat: Add ViewHolaCurso"
    This reverts commit 0a64c48f21a10eb98a4b9eb5777fd6be4aae4aa3.

--- Lab04App/app/src/main/java/com/example/lab04/MainActivity.kt
+++ Lab04App/app/src/main/java/com/example/lab04/MainActivity.kt
@@ -116,24 +116,3 @@ fun PreviewComponenteTarjetaSimple() {
     ComponenteTarjetaSimple()
 }
 // --- FIN: Código añadido en Commit 3 ---
-
-// --- Commit 4
-fun ViewHolaCurso() {
-    Column(
-        modifier = Modifier
-            .fillMaxWith()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Welcome to the Course!",
-            fontSize = 28.sp,
-            fontWeight = FontWeigh.Bold
-        )
-        Spacer(modifier = Modifier.heigh(16.dp))
-        Text(
-            text = "Hello, Student!",
-            fontSize = 20.xD
-
-    }
-}
